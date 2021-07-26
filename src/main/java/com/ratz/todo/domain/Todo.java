ffa1b629332c04decf25bddcad8921b0e38c0037
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-07-26 08:41:39 +0000

    finished the domain class

--- src/main/java/com/ratz/todo/domain/Todo.java
+++ src/main/java/com/ratz/todo/domain/Todo.java
@@ -1,14 +1,104 @@
 package com.ratz.todo.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Todo {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Todo implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String description;
 	private LocalDateTime dateToFinish;
 	private Boolean finished = false;
 	
+	public Todo() {
+		super();
+	}
 
+	public Todo(Integer id, String title, String description, LocalDateTime dateToFinish, Boolean finished) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.dateToFinish = dateToFinish;
+		this.finished = finished;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDateToFinish() {
+		return dateToFinish;
+	}
+
+	public void setDateToFinish(LocalDateTime dateToFinish) {
+		this.dateToFinish = dateToFinish;
+	}
+
+	public Boolean getFinished() {
+		return finished;
+	}
+
+	public void setFinished(Boolean finished) {
+		this.finished = finished;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+  
 }
