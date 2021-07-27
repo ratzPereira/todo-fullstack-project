bb0ec3000ec3709cbef23ee3a5e1be6414f06549
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-07-27 09:37:28 +0000

    created footer component,created read-all component and added styles.

--- front/todo/src/app/app.module.ts
+++ front/todo/src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from "./components/footer/footer.component";
+import { ReadAllComponent } from "./components/read-all/read-all.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ReadAllComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-      MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
