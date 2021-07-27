391467dc6be902bcffca75da6238539c6f835d1d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-07-27 09:53:57 +0000

    created todo model, created dummy to-do array to render and test our todo components

--- front/todo/src/app/components/read-all/read-all.component.ts
+++ front/todo/src/app/components/read-all/read-all.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Todo } from "../../models/todo";
 
 @Component({
   selector: "app-read-all",
@@ -6,6 +7,19 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./read-all.component.css"],
 })
 export class ReadAllComponent implements OnInit {
+  list: Todo[] = [
+    {
+      title: "Olá",
+      dateToFinish: new Date(),
+      finished: false,
+    },
+    {
+      title: "Olá Again",
+      dateToFinish: new Date(),
+      finished: true,
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
